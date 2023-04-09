80f9a583028eeb1c903dfe89d73b7bd0b7ab7ec8
akashsuman <akash.suman@nagarro.com>
2023-04-09 11:03:13 +0530

    initial

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("Demo Test Case");
+        }
+    }
+}
